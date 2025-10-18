2b82475fe8b598f2279920ed3a9be75c5bf9c2ed
seanthompson830-svg <seanthompson830@gmail.com>
2025-10-17 22:24:40 -0500

    Added tic-tac-toe.js and implemented board layout

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('#board div');
+  squares.forEach(square => {
+    square.classList.add('square');
+  });
+});
